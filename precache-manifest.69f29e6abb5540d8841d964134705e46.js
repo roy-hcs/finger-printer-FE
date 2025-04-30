e95f117872c20a6edc24c9cd53972545816c45b0
roy-hcs <chenxingying@hcs55.com>
2025-04-30 10:42:31 +0800

    Updates

---
+++ precache-manifest.69f29e6abb5540d8841d964134705e46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2511c4871ac61a87147d99ebbc3a8f38",
+    "url": "404.html"
+  },
+  {
+    "revision": "049d8ca747e3696c5f4b",
+    "url": "css/app.9c54c7ec.css"
+  },
+  {
+    "revision": "d97d5b3ec2c768ca842c59827c075378",
+    "url": "index.html"
+  },
+  {
+    "revision": "049d8ca747e3696c5f4b",
+    "url": "js/app.5fe36d7b.js"
+  },
+  {
+    "revision": "ecfe6b02848710daa039",
+    "url": "js/chunk-vendors.b7c7b14c.js"
+  },
+  {
+    "revision": "79eb98cd568712764a0bd9dd48adf08c",
+    "url": "manifest.json"
+  }
+]);
